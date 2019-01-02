d768232819bc6559f6b34643d51ef6a39466c998
Jaybird <jrmiller022@gmail.com>
2019-01-01 19:06:39 -0600

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,6 +8,7 @@ $("button").on("click", function(){
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     x + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+//AJAX call function.
     $.ajax({url:queryURL,method:"GET"})
         .done(function(response){
         for(var i = 0; i<response.data.length; i++){
@@ -18,6 +19,24 @@ $("button").on("click", function(){
             vehicalDiv.prepend(p);
             vehicalDiv.prepend(vechalImage);
             $("#gifArea").prepend(vehicalDiv); 
-        }
+    }
+})
+
+//Create an IF statement for still.
+$("#gifArea").on("click", function() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+      
+//Check if the variable state is equal to still
+    if (state === "still") {
+      var urlForAnimatedImage = $(this).attr("data-animate");
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else if (state === "animate") {
+        var urlForStillImage = $(this).attr("data-still");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
     })
 })
