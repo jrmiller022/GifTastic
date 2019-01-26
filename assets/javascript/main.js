d671e4969ba090d52dd3f0fb8333aa30a429299a
Jaybird <jrmiller022@gmail.com>
2019-01-26 17:44:25 -0600

    fixed bugs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,72 +1,69 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Variable of Topics.
+    //Variable of Topics.
 
-var topics = ["Jeep Wrangler", "Chevrolet", "Ford", "Nissan", "Honda"];
+    var topics = ["Jeep Wrangler", "Chevrolet", "Ford", "Nissan", "Honda"];
 
-//User clicks buttons to generate gifs.
-$("button").on("click", function(){
+    //User clicks buttons to generate gifs.
+    $("button").on("click", function () {
 
-    var topic = $(this).data("vehicle");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var topic = $(this).data("vehicle");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-//AJAX call function.
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response){
-        
-    for(var i = 0; i<response.data.length; i++) {
-        var vehicleDiv = $("<div>");
+        //AJAX call function.
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
 
-//Rating setup
-    var p = $("<p>").text("Rating: "+response.data[i].rating);
-    var vehicleImage = $("<img>");
+            for (var i = 0; i < response.data.length; i++) {
+                var vehicleDiv = $("<div>");
 
-//Image still
-    vehicleImage.attr("data-still", response.data[i].images.fixed_height_still.url);
-    vehicleImage.attr("data-state", "still");
-    
-//Image animate
-    vehicleImage.attr("src", response.data[i].images.fixed_height.url);
-    vehicleImage.attr("data-animate", response.data[i].images.fixed_height.url);
+                //Rating setup
+                var p = $("<p>").text("Rating: " + response.data[i].rating);
+                var vehicleImage = $("<img>");
 
-    vehicleDiv.append(p);
-    vehicleDiv.append(vehicleImage);
-    $("#gifArea").prepend(vehicleDiv); 
+                //Image still
+                vehicleImage.attr("data-still", response.data[i].images.fixed_height_still.url);
+                vehicleImage.attr("data-state", "still");
 
-    }
+                //Image animate
+                vehicleImage.attr("src", response.data[i].images.fixed_height_still.url);
+                vehicleImage.attr("data-animate", response.data[i].images.fixed_height.url);
+
+                vehicleDiv.append(p);
+                vehicleDiv.append(vehicleImage);
+                $("#gifArea").prepend(vehicleDiv);
+
+            }
+        })
     })
-})
 
-// Add your vehicle
-$("#add-vehicle").on("click", function(event){
-        
-    event.preventDefault();
+    // Add your vehicle
+    $("#add-vehicle").on("click", function (event) {
 
-    var topic = $("#vehicle-input").val().trim();
-    $("#add-vehicle").val("");
-    topics.push(topic);
+        event.preventDefault();
 
-    showButtons();
-});
+        var topic = $("#vehicle-input").val().trim();
+        $("#add-vehicle").val("");
+        topics.push(topic);
 
-$(document).on("click", "#gifArea", pauseGif);
+        showButtons();
+    });
 
-//Create an IF statement for still.
-function pauseGif() {
+    $(document).on("click", "img", moveGif);
 
+    //Create an IF statement for still.
+    function moveGif() {
+        var state = $(this).attr('data-state');
         if (state === "still") {
-            console.log(state);
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
-    }
+        }
         else if (state === "animate") {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
-    }
+        }
 
     }
-
 });
- 
