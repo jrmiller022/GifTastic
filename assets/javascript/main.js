c88af91f95a6cc725be3b1f0666bc6217f64fcd7
Jaybird <jrmiller022@gmail.com>
2019-01-29 19:22:53 -0600

    update fix on side search bar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,69 +1,83 @@
 $(document).ready(function () {
 
     //Variable of Topics.
-
     var topics = ["Jeep Wrangler", "Chevrolet", "Ford", "Nissan", "Honda"];
 
-    //User clicks buttons to generate gifs.
-    $("button").on("click", function () {
+    function showButtons() {
 
-        var topic = $(this).data("vehicle");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+        $("#buttons-view").empty();
 
-        //AJAX call function.
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function (response) {
+        for (var i = 0; i < topics.length; i++) {
 
-            for (var i = 0; i < response.data.length; i++) {
-                var vehicleDiv = $("<div>");
+            var topicButton = $("<button>");
+            topicButton.addClass("btn-btn-info");
+            topicButton.attr("data-name", topics[i]);
+            topicButton.text(topics[i]);
+            $("#buttons-view").append(topicButton);
 
-                //Rating setup
-                var p = $("<p>").text("Rating: " + response.data[i].rating);
-                var vehicleImage = $("<img>");
-
-                //Image still
-                vehicleImage.attr("data-still", response.data[i].images.fixed_height_still.url);
-                vehicleImage.attr("data-state", "still");
-
-                //Image animate
-                vehicleImage.attr("src", response.data[i].images.fixed_height_still.url);
-                vehicleImage.attr("data-animate", response.data[i].images.fixed_height.url);
-
-                vehicleDiv.append(p);
-                vehicleDiv.append(vehicleImage);
-                $("#gifArea").prepend(vehicleDiv);
-
-            }
-        })
-    })
-
+        }
+        $("#gifs").empty();
+    }
     // Add your vehicle
-    $("#add-vehicle").on("click", function (event) {
+    $("#add-Vehicle").on("click", function (event) {
 
         event.preventDefault();
 
         var topic = $("#vehicle-input").val().trim();
-        $("#add-vehicle").val("");
+        $("#vehicle-input").val("");
         topics.push(topic);
 
         showButtons();
     });
+})
+
+    //User clicks buttons to generate gifs.
+    $("button").on("click", function () {
+
+    var topic = $(this).data("vehicle");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    //AJAX call function.
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+
+        for (var i = 0; i < response.data.length; i++) {
+            var vehicleDiv = $("<div>");
+
+            //Rating setup
+            var p = $("<p>").text("Rating: " + response.data[i].rating);
+            var vehicleImage = $("<img>");
+
+            //Image still
+            vehicleImage.attr("data-still", response.data[i].images.fixed_height_still.url);
+            vehicleImage.attr("data-state", "still");
+
+            //Image animate
+            vehicleImage.attr("src", response.data[i].images.fixed_height_still.url);
+            vehicleImage.attr("data-animate", response.data[i].images.fixed_height.url);
+
+            vehicleDiv.append(p);
+            vehicleDiv.append(vehicleImage);
+            $("#gifArea").prepend(vehicleDiv);
+
+        }
+    })
+})
+
 
     $(document).on("click", "img", moveGif);
-
-    //Create an IF statement for still.
-    function moveGif() {
-        var state = $(this).attr('data-state');
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate");
-        }
-        else if (state === "animate") {
-            $(this).attr("src", $(this).attr("data-still"));
-            $(this).attr("data-state", "still");
-        }
-
+//Create an IF statement for still.
+function moveGif() {
+    var state = $(this).attr('data-state');
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     }
-});
+    else if (state === "animate") {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+
+}
